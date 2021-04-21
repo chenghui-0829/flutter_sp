dd0b4aae9ac28c914ba985debaa4359d716c56d1
chenghui <chenghui@bobo.so>
2021-04-21 10:11:04 +0800

    支持空安全

--- lib/flutter_sp.dart
+++ lib/flutter_sp.dart
@@ -6,11 +6,11 @@ class FlutterSp {
   static const MethodChannel _channel =
       const MethodChannel('io.flutter.plugins/flutter_sp');
 
-  static Future<String> getUid() async {
-    return _channel.invokeMethod('get_uid');
+  static Future<String?> getUid() async {
+    return _channel.invokeMethod<String>('get_uid');
   }
 
-  static Future<String> clearUid() async {
-    return _channel.invokeMethod('set_uid');
+  static Future<String?> clearUid() async {
+    return _channel.invokeMethod<String>('set_uid');
   }
 }
